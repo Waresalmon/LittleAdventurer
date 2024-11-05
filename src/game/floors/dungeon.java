8b116b560e3180303b00fadc3eb664065423795a
Waresalmon <86568693+Waresalmon@users.noreply.github.com>
2024-11-05 14:53:22 -0700

    updated dungeon and f1

--- src/game/floors/dungeon.java
+++ src/game/floors/dungeon.java
@@ -1,21 +1,47 @@
 package game.floors;
 
+import game.enemies.enemies;
 import game.player.player;
+import java.util.ArrayList;
 
 public class dungeon {
-  private boolean bossDefeated;
-  private boolean adventurerAlive;
-
-
-  public dungeon(){
-    bossDefeated = false;
-    adventurerAlive = true;
-  }
-  public static void main(String[] args) {
-    dungeon current = new f1(); //creates new floor 1
-    player player = new player(); 
-    while(!bossDefeated){
+  protected static boolean bossDefeated;    //Had to make static is there a way to avoid this?
+  protected static boolean adventurerAlive; //Had to make static is there a way to avoid this?
+  protected ArrayList<enemies> enemyList;
+  protected enemies boss;
+  
+    public dungeon(){
+      bossDefeated = false;
+      adventurerAlive = true;
+      enemyList = new ArrayList<>();
       
+    }
+    public String encounter(enemies e){
+      String s;
+      s = "You have encounter a " + e.getClass().getSimpleName(); //get class name of enemy randomly from the list
+      return s;
+
+    }
+    //insertion is done by the floor class
+    public enemies selectRandomEnemy(){
+      int index = (int) (Math.random() * enemyList.size());
+      return enemyList.get(index);
+    }
+    
+    public void removeEnemy(enemies e){
+      enemyList.remove(e); //should be by reference
+    }
+    public static void main(String[] args) {
+      dungeon currentD = new f1(); //creates new floor 1. player must clear f1 before going to f2
+      //doesn'd intialize dungeon constructor will insert use f1() constructor
+      player player = new player(); //creates player
+      enemies nextEnemy = currentD.selectRandomEnemy();
+
+      System.out.println("Welcome adventurer");
+      while(bossDefeated == false && adventurerAlive == true){
+        //set up encouter
+        System.out.println(currentD.encounter(nextEnemy));
+        
     }
 
   }
--- src/game/floors/f1.java
+++ src/game/floors/f1.java
@@ -4,10 +4,24 @@ import game.enemies.*;
 import java.util.ArrayList;
 
 public class f1 extends dungeon {
-  ArrayList<enemies> enemies;
+  private int goblinCount;
+  private int bossCount;
+  
   public f1(){
-    ArrayList<enemies> enemies = new ArrayList<enemies>();
-
+    goblinCount = 10;
+    bossCount = 1;
+    enemyList = new ArrayList<enemies>();
+    //insertEnemies(); //good idea?, bad idea?
   }
+  public void insertEnemies(){
+    //Floors only contains 10 goblins and 1 boss
+    for(int i = 0; i < goblinCount; i++){
+      enemies g = new goblin();
+      enemyList.add(g);
+    }
+    boss = new goblinKing();
+  } 
 
+
+  
 }
